ef260a972386993bf5557236a21ba7c79a48f5ef
Asnababar <147834277+Asnababar@users.noreply.github.com>
2024-02-17 21:15:15 +0500

    Node.js Q7

---
+++ Q7.js
@@ -0,0 +1,10 @@
+"use strict";
+// Number eight write addition, substraction, multiplication and division operation that each results in the number 8. be sure to enclose your operation in print statement to see the result.. 
+Object.defineProperty(exports, "__esModule", { value: true });
+// Addition
+let a = 10;
+let b = 5;
+console.log(`${a} + ${b}\=${a + b}`);
+console.log(`${a} - ${b}\=${a - b}`);
+console.log(`${a} * ${b}\=${a * b}`);
+console.log(`${a} / ${b}\=${a / b}`);
---
+++ Q7.ts
@@ -0,0 +1,18 @@
+// Number eight write addition, substraction, multiplication and division operation that each results in the number 8. be sure to enclose your operation in print statement to see the result.. 
+
+// Addition
+
+let a: number = 10
+let b: number = 5
+
+console.log(`${a} + ${b}\=${a + b}`);
+console.log(`${a} - ${b}\=${a - b}`);
+console.log(`${a} * ${b}\=${a * b}`);
+console.log(`${a} / ${b}\=${a / b}`);
+
+
+
+
+
+
+
